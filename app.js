4063fc8c8b2779827d611f62dc8337ce86548962
Rorbak <rikke.rorbak@gmail.com>
2022-11-01 20:21:36 +0100

    changed called api url

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-const midtownContent = document.querySelector('#midtown-frame-container');
-const campusContent = document.querySelector('#campus-frame-container');
+const midtownContent = document.querySelector('div.menu-frame.midtown');
+const campusContent = document.querySelector('div.menu-frame.campus');
 const iconArray = ['../src/assets/icons/hot.png', '../src/assets/icons/diet.png', '../src/assets/icons/salad.png'];
-// const appUrl = (process.env.NODE_ENV)
 
 async function getMidTownMenu() {   //consider using async/await instead of promises
-   fetch('https://lh-lunch-api.onrender.com/midtownmenu')   //
+   fetch('https://lh-lunch-api.azurewebsites.net/midtownmenu')   //
       .then(response => { return response.json() }) //return response.json()
       .then(data => {
          console.log(data);
          // console.log(appUrl);   //check return type
          if (!data.length) {
-            console.log('there is no data!');
+            console.log('There is no data!');
             if (midtownContent.childElementCount < 2) {
-               const noMenuMsg = `<div class="menuItem"><h2>Intet på menuen i dag</h2></div>`
+               const noMenuMsg = `<div class="menu-item no-menu"><h2>Intet på menuen i dag</h2></div>`
                midtownContent.insertAdjacentHTML('beforeend', noMenuMsg);
             }
          } else {
             data.forEach(item => {
-               const menuItem = `<div class="menuItem"><h3>${item.title}</h3><p>${item.description}</p></div>`
+               const menuItem = `<div class="menu-item"><h3>${item.title}</h3><p>${item.description}</p></div>`
                midtownContent.insertAdjacentHTML('beforeend', menuItem)
             })
          }
@@ -26,18 +25,18 @@ async function getMidTownMenu() {   //consider using async/await instead of prom
 }
 
 async function getCampusMenu() {
-   fetch('https://lh-lunch-api.onrender.com/campusmenu')   //
+   fetch('https://lh-lunch-api.azurewebsites.net//campusmenu')   //
       .then(response => { return response.json() })
       .then(data => {
          if (!data.length) {
             console.log('There is no data!');
             if (campusContent.childElementCount < 1) {
-               const noMenuMsg = `<div class="menuItem"><h2>Intet på menuen i dag</h2></div>`
+               const noMenuMsg = `<div class="menu-item no-menu"><h2>Intet på menuen i dag</h2></div>`
                campusContent.insertAdjacentHTML('beforeend', noMenuMsg);
             }
          } else {
             data.forEach(item => {
-               const menuItem = `<div class="menuItem"><h3>${item.title}</h3><p>${item.description}</p></div>`
+               const menuItem = `<div class="menu-item"><h3>${item.title}</h3><p>${item.description}</p></div>`
                campusContent.insertAdjacentHTML('beforeend', menuItem)
             })
          }
